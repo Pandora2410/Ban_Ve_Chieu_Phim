1cc6394e5f0c5a590e8d703832c48889aa6563a4
Admin <Admin@PC-ADMIN>
2025-11-29 15:05:19 +0700

    Add project files.

---
+++ Ban_Ve_Chieu_Phim/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace Ban_Ve_Chieu_Phim
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel_GHE = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_tienve = new System.Windows.Forms.TextBox();
+            this.btn_dat = new System.Windows.Forms.Button();
+            this.btn_huy = new System.Windows.Forms.Button();
+            this.btn_end = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Firebrick;
+            this.label1.Location = new System.Drawing.Point(331, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(219, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "RẠP CHIẾU PHIM";
+            // 
+            // panel_GHE
+            // 
+            this.panel_GHE.Location = new System.Drawing.Point(12, 81);
+            this.panel_GHE.Name = "panel_GHE";
+            this.panel_GHE.Size = new System.Drawing.Size(897, 308);
+            this.panel_GHE.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(525, 423);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Tổng tiền vé";
+            // 
+            // txt_tienve
+            // 
+            this.txt_tienve.Location = new System.Drawing.Point(682, 421);
+            this.txt_tienve.Name = "txt_tienve";
+            this.txt_tienve.Size = new System.Drawing.Size(227, 22);
+            this.txt_tienve.TabIndex = 3;
+            // 
+            // btn_dat
+            // 
+            this.btn_dat.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_dat.Location = new System.Drawing.Point(197, 489);
+            this.btn_dat.Name = "btn_dat";
+            this.btn_dat.Size = new System.Drawing.Size(146, 37);
+            this.btn_dat.TabIndex = 4;
+            this.btn_dat.Text = "Đặt Vé";
+            this.btn_dat.UseVisualStyleBackColor = true;
+            this.btn_dat.Click += new System.EventHandler(this.btn_dat_Click);
+            // 
+            // btn_huy
+            // 
+            this.btn_huy.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_huy.Location = new System.Drawing.Point(379, 489);
+            this.btn_huy.Name = "btn_huy";
+            this.btn_huy.Size = new System.Drawing.Size(147, 37);
+            this.btn_huy.TabIndex = 5;
+            this.btn_huy.Text = "Hủy Vé";
+            this.btn_huy.UseVisualStyleBackColor = true;
+            this.btn_huy.Click += new System.EventHandler(this.btn_huy_Click);
+            // 
+            // btn_end
+            // 
+            this.btn_end.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_end.Location = new System.Drawing.Point(547, 489);
+            this.btn_end.Name = "btn_end";
+            this.btn_end.Size = new System.Drawing.Size(152, 37);
+            this.btn_end.TabIndex = 6;
+            this.btn_end.Text = "Kết Thúc";
+            this.btn_end.UseVisualStyleBackColor = true;
+            this.btn_end.Click += new System.EventHandler(this.btn_end_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(921, 582);
+            this.Controls.Add(this.btn_end);
+            this.Controls.Add(this.btn_huy);
+            this.Controls.Add(this.btn_dat);
+            this.Controls.Add(this.txt_tienve);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel_GHE);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel_GHE;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_tienve;
+        private System.Windows.Forms.Button btn_dat;
+        private System.Windows.Forms.Button btn_huy;
+        private System.Windows.Forms.Button btn_end;
+    }
+}
+
---
+++ Ban_Ve_Chieu_Phim/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ban_Ve_Chieu_Phim
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_dat_Click(object sender, EventArgs e)
+        {
+            string tienve = txt_tienve.Text;
+            var a = MessageBox.Show($"Tong tien ve la : {tienve}." +
+                "Ban co muon thanh toan?", "Thong Bao",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+            if (a == DialogResult.Yes)
+            {
+                foreach (Control c in panel_GHE.Controls)
+                {
+                    if (c is Button)
+                    {
+                        Button btn = (Button)c;
+                        if (btn.BackColor == Color.Yellow)
+                        {
+                            btn.BackColor = Color.Aqua;
+                        }
+                    }
+
+                }
+                txt_tienve.Text = "0";
+            }
+
+        }
+
+        private void btn_huy_Click(object sender, EventArgs e)
+        {
+            foreach (Control c in panel_GHE.Controls)
+            {
+                if (c is Button)
+                {
+                    Button btn = (Button)c;
+                    if (btn.BackColor == Color.Yellow)
+                    {
+                        btn.BackColor = Color.White;
+                    }
+                }
+            }
+            txt_tienve.Text = "0";
+        }
+        private void btn_GHE_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            if (btn.BackColor == Color.White)
+            {
+                btn.BackColor = Color.Yellow;
+            }
+            else if (btn.BackColor == Color.Yellow)
+            {
+                btn.BackColor = Color.White;
+            }
+            else if (btn.BackColor == Color.Aqua)
+            {
+                MessageBox.Show("Ghế đã được đặt, vui lòng chọn ghế khác", "Thông báo",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            Tinh_Tienve();
+        }
+
+        private void btn_end_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            this.Close();
+
+        }
+
+        private void Tinh_Tienve()
+        {
+            List<double> gia = new List<double>()
+            { 100000, 120000, 150000, 180000, 200000 };
+            double tongtien = 0;
+            foreach (Control c in panel_GHE.Controls)
+            {
+                if (c is Button)
+                {
+                    Button btn = (Button)c;
+                    if (btn.BackColor == Color.Yellow)
+                    {
+                        int hang = btn.Name[4] - '0';
+                        if (hang < gia.Count)
+                        {
+                            tongtien += gia[hang];
+                        }
+                        else
+                            tongtien += gia[gia.Count - 1];
+                    }
+                }
+            }
+            txt_tienve.Text = tongtien.ToString("N0");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int so_hang = 4, so_cot = 6;
+            int chdai_ghe = 50, chcao_ghe = 50;
+            int kcach = 10;
+            for (int i = 0; i < so_hang; i++)
+            {
+                for (int j = 0; j < so_cot; j++)
+                {
+                    Button btn = new Button();
+                    btn.Size = new System.Drawing.Size(chdai_ghe, chcao_ghe);
+                    btn.Location = new System.Drawing.Point(
+                        j * (chdai_ghe + kcach) + 12,
+                        i * (chcao_ghe + kcach) + 12);
+                    btn.BackColor = Color.White;
+                    btn.Name = $"btn_{i}_{j}";
+                    btn.Text = (char)('A' + i) + (j + 1).ToString();
+                    btn.Click += new System.EventHandler(this.btn_GHE_Click);
+                    panel_GHE.Controls.Add(btn);
+
+                }
+            }
+
+            // chinh lai size cho panel
+            panel_GHE.Size = new System.Drawing.Size(
+                so_cot * (chdai_ghe + kcach) + 14,
+                so_hang * (chcao_ghe + kcach) + 14
+                );
+
+            //chinh lai form
+            this.Size = new System.Drawing.Size(
+                panel_GHE.Size.Width + 60,
+                panel_GHE.Size.Height + 200
+                );
+
+            // Doi chu man anh
+            label1.Left = (this.Width - label1.Width) / 2;
+
+            //Doi chu tong tien ve
+            txt_tienve.Left = panel_GHE.Width - txt_tienve.Width - 12;
+            txt_tienve.Top = panel_GHE.Bottom + 16;
+            label2.Left = txt_tienve.Left - label2.Width - 10;
+            label2.Top = txt_tienve.Top + 4;
+
+            // doi 3 nut bam
+            //btn_huy.Left = this.Width / 2 - btn_huy.Width;
+            btn_huy.Left  = panel_GHE.Right -btn_end.Width - btn_huy.Width -15;
+            btn_huy.Top = panel_GHE.Bottom + 60;
+            btn_dat.Left = btn_huy.Left - btn_dat.Width  -20;
+            btn_dat.Top = btn_huy.Top;
+
+            btn_end.Left = panel_GHE.Right -btn_end.Width;
+            btn_end.Top = btn_huy.Top;
+        }
+    } 
+    }
